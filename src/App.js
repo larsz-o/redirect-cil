511ada45cc62a47fcbf943d1f2cbea554f596a67
Lars Mackenzie <larsmackenzie@gmail.com>
2021-01-28 16:49:03 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './bear.svg';
 import './App.css';
 
 function App() {
@@ -7,16 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+         Looking for the <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://cil-policy.herokuapp.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Chaplaincy Innovation Lab lessons?
+        </a> </p>
       </header>
     </div>
   );
